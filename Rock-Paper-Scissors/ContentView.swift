f63829504ac248cc9b9f7a6dd78ffb5693cb4c71
Tugran Yayla <tugrandev@gmail.com>
2023-03-25 16:16:30 +0300

    All the modifications	are done.

--- Rock-Paper-Scissors/ContentView.swift
+++ Rock-Paper-Scissors/ContentView.swift
@@ -8,14 +8,113 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var rpsArray = ["🪨", "📄", "✂️"]
+    @State private var winOrLose = ["Win", "Lose"]
+    @State private var alertTitle = ""
+    @State private var showAlertofScore = false
+    @State private var score = 0
+    @State private var appsChoice = Int.random(in: 0...2)
+    @State private var questionCount = 1
+    @State private var winOrLoseChoice = Int.random(in: 0...1)
+    @State private var showResult = false
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(colors: [.green, .yellow], startPoint: .bottom, endPoint: .top)
+                .ignoresSafeArea()
+            VStack(spacing: 40) {
+                Spacer()
+                Text("App's choice is")
+                    .font(.largeTitle.bold())
+                Text("\(rpsArray[appsChoice])")
+                    .font(.system(size: 200))
+                Text("Choose a move to \(winOrLose[winOrLoseChoice])")
+                    .foregroundColor(.secondary)
+                    .font(.title3.bold())
+                HStack(spacing: 20) {
+                    ForEach(0..<3) { number in
+                        Button("\(rpsArray[number])") {
+                            if winOrLose[winOrLoseChoice] == "Win" {
+                                buttonTappedWin(number)
+                            } else {
+                                buttonTappedLose(number)
+                            }
+                        }
+                        .frame(maxWidth: .infinity)
+                        .padding(.vertical, 5)
+                        .background(.thinMaterial)
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                        .font(.system(size: 75))
+                    }
+                }
+                Text("Score: \(score)")
+                    .font(.largeTitle.bold())
+                Text("\(questionCount)/10")
+                    .font(.title3.bold())
+                    .foregroundColor(.secondary)
+                Spacer()
+            }
+            .padding()
+            .alert(Text(alertTitle), isPresented: $showAlertofScore) {
+                Button("Next", action: newQuestion)
+            }
+            .alert(Text("Game over!"), isPresented: $showResult) {
+                Button("New Game", action: newGame)
+            } message: {
+                Text("Your score was \(score)")
+            }
         }
-        .padding()
+        
+    }
+    func buttonTappedLose(_ number: Int) {
+        if rpsArray[appsChoice] == "🪨" && rpsArray[number] == "✂️" {
+            alertTitle = "Correct"
+            score += 10
+        } else if rpsArray[appsChoice] == "📄" && rpsArray[number] == "🪨" {
+            alertTitle = "Correct"
+            score += 10
+        } else if rpsArray[appsChoice] == "✂️" && rpsArray[number] == "📄" {
+            alertTitle = "Correct"
+            score += 10
+        } else {
+            alertTitle = "False"
+            score -= 10
+        }
+        if questionCount == 10 {
+            showResult = true
+        } else {
+            showAlertofScore = true
+        }
+    }
+    func buttonTappedWin(_ number: Int) {
+        if rpsArray[appsChoice] == "🪨" && rpsArray[number] == "📄" {
+            alertTitle = "Correct"
+            score += 10
+        } else if rpsArray[appsChoice] == "📄" && rpsArray[number] == "✂️" {
+            alertTitle = "Correct"
+            score += 10
+        } else if rpsArray[appsChoice] == "✂️" && rpsArray[number] == "🪨" {
+            alertTitle = "Correct"
+            score += 10
+        } else {
+            alertTitle = "False"
+            score -= 5
+        }
+        if questionCount == 10 {
+            showResult = true
+        } else {
+            showAlertofScore = true
+        }
+    }
+    func newQuestion() {
+        appsChoice = Int.random(in: 0...2)
+        winOrLoseChoice = Int.random(in: 0...1)
+        questionCount += 1
+    }
+    func newGame() {
+        questionCount = 0
+        score = 0
+        newQuestion()
     }
 }
 
